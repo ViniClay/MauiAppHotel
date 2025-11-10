c30091b13a2976f9d9b9024c280fdcb61f5807fe
Vini Clay <viniviniciusclay07@gmail.com>
2025-11-09 23:43:37 -0300

    Botão e tela Sobre; Ajuste e criação de cores padrão

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,41 @@
-﻿namespace MauiAppHotel
+﻿using MauiAppHotel.Models;
+
+namespace MauiAppHotel
 {
     public partial class App : Application
-    {
+    {   
+        public List<Quarto> Lista_quartos = new List<Quarto>
+        {
+            new Quarto
+            {
+                Descricao = "Suíte Super Luxo",
+                ValorDiariaAdulto = 110.00,
+                ValorDiariaCrianca = 55.00
+            },
+            new Quarto
+            {
+                Descricao = "Suíte Luxo",
+                ValorDiariaAdulto = 80.00,
+                ValorDiariaCrianca = 40.00
+            },
+            new Quarto
+            {
+                Descricao = "Suíte Single",
+                ValorDiariaAdulto = 50.00,
+                ValorDiariaCrianca = 25.00
+            },
+            new Quarto
+            {
+                Descricao = "Quarto Crise",
+                ValorDiariaAdulto = 25.00,
+                ValorDiariaCrianca = 12.50
+            },
+        };
         public App()
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new Views.Sobre());
+            MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
--- Views/ContratacaoHospedagem.xaml.cs
+++ Views/ContratacaoHospedagem.xaml.cs
@@ -2,8 +2,51 @@ namespace MauiAppHotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
 {
+	App PropriedadesApp;
 	public ContratacaoHospedagem()
 	{
 		InitializeComponent();
-	}
+
+		PropriedadesApp = (App)Application.Current;
+
+		pck_quarto.ItemsSource = PropriedadesApp.Lista_quartos;
+
+		dtpck_checkin.MinimumDate = DateTime.Now;
+		dtpck_checkin.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
+
+		dtpck_checkout.MinimumDate = ((DateTime)dtpck_checkin.Date).AddDays(1);
+		dtpck_checkout.MaximumDate = ((DateTime)dtpck_checkin.Date).AddMonths(6);
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Navigation.PushAsync(new HospedagemContratada());
+		} catch (Exception ex)
+		{
+			DisplayAlertAsync("Ops", ex.Message, "OK");
+		}
+    }
+
+    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+		DatePicker elemento = sender as DatePicker;
+
+		DateTime data_selecionada_checkin = elemento.Date.Value;
+		dtpck_checkout.MinimumDate = data_selecionada_checkin.AddDays(1);
+		dtpck_checkout.MaximumDate = data_selecionada_checkin.AddMonths(6);
+    }
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+        try
+        {
+            Navigation.PushAsync(new Sobre());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlertAsync("Ops", ex.Message, "OK");
+        }
+    }
 }
